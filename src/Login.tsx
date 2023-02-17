efd789e2be48bd56fe06d8534bbdd2f4a50f5243
Timo Hermans <tmhermans@gmail.com>
2023-02-17 08:19:23 +0100

    Style the login a bit

--- src/Login.tsx
+++ src/Login.tsx
@@ -23,24 +23,36 @@ export function Login({ onLogin }: { onLogin: () => void }) {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div className="h-screen bg-gradient-to-br from-cyan-500 to-blue-900 p-4">
+      <form
+        className="mx-auto flex w-96 flex-col gap-4 rounded-xl bg-sky-800 p-4 text-white"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         {message && <p>{message}</p>}
-        <p>
-          <label>Username</label>
-          <input type="text" {...register("username", { required: true })} />
+        <p className="flex flex-col">
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            placeholder=""
+            type="text"
+            className="px-2 h-8 rounded-xl leading-8 text-black"
+            {...register("username", { required: true })}
+          />
           {errors.username && <span>!</span>}
         </p>
-        <p>
-          <label>Password</label>
+        <p className="flex flex-col">
+          <label htmlFor="password">Password</label>
           <input
+            id="password"
             type="password"
+            placeholder=""
+            className="px-2 h-8 rounded-xl leading-8 text-black"
             {...register("password", { required: true })}
           />
           {errors.password && <span>!</span>}
         </p>
         <p>
-          <button type="submit">Log in</button>
+          <button className="bg-yellow-400 rounded-sm h-8 w-20 text-black" type="submit">Log in</button>
         </p>
       </form>
     </div>
